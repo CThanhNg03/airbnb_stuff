500c744cbab2f576928728ff8370e71a41b09047
Ng Cao Thanh <cthanhnguyen03@gmail.com>
2024-04-02 12:53:08 +0700

    add auth and user module

--- app/social-service/src/module/app.module.ts
+++ app/social-service/src/module/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '../controller/app.controller';
 import { AppService } from '../service/app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user.module';
+import { AuthModule } from './auth.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://mongodb:27017/social'), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
